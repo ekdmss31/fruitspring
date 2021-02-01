33d26dd5b64ee22beaacb37ff8bbd52f10a397ab
ekdmss31 <57400849+ekdmss31@users.noreply.github.com>
2021-02-01 22:21:33 +0900

    1st commit

---
+++ src/main/java/Project/fruitspring/FruitspringApplication.java
@@ -0,0 +1,13 @@
+package Project.fruitspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/fruitspring/FruitspringApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.fruitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
