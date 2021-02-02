18264c66df962936be2e5f64ec755b36185aa5e6
ekdmss31 <57400849+ekdmss31@users.noreply.github.com>
2021-02-02 23:12:13 +0900

    2nd commit

---
+++ src/main/java/Project/fruitspring/controller/LoginController.java
@@ -0,0 +1,24 @@
+package Project.fruitspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+
+    @GetMapping("login")
+    public String login(Model model) {
+        model.addAttribute("data","hello!!");
+        return "Login";
+    }
+
+    //MVC
+    @GetMapping("login-mvc")
+    public String loginMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "Login";
+    }
+}
