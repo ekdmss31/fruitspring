9ad05dd9f6bc4797ac29475e49ceadb23a784cdd
ekdmss31 <57400849+ekdmss31@users.noreply.github.com>
2021-02-08 00:15:33 +0900

    9th commit

--- src/main/java/Project/fruitspring/controller/LoginController.java
+++ src/main/java/Project/fruitspring/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -26,5 +27,24 @@ public class LoginController {
     @GetMapping(value = "/sign")
     public String signFrom(){
         return "sign";
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //문자가 아닌 객체를 넘김. 결과값이 { name: "spring!!" } 이렇게 뜸. 그런데 이것은 json 방식임. {"key : value"}로 이루어져있음.
+
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
